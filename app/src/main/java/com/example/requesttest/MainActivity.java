c15feb8b0d8d82057ae84ea38588d788d172b131
Emilija Krstonošić <emilija@Dragans-iMac.local>
2020-01-10 22:00:52 +0100

    fixed login fucking java i hate it

--- app/src/main/java/com/example/requesttest/MainActivity.java
+++ app/src/main/java/com/example/requesttest/MainActivity.java
@@ -17,9 +17,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
@@ -71,41 +73,54 @@ public class MainActivity extends AppCompatActivity {
 
     private void loginFunction(final EditText loginUsername, final EditText loginPassword, final TextView loginStatus,final RequestQueue requestQueue) {
 
-        String URL = "https://foodpicker-api-deploy.herokuapp.com/users/1";
+        String URL = "https://foodpicker-api-deploy.herokuapp.com/users/name/"+loginUsername.getText().toString();
+        Log.i("REST: ", "fetching data from "+URL);
 
 
-
-        JsonObjectRequest objectRequest=new JsonObjectRequest(
+        JsonArrayRequest objectRequest=new JsonArrayRequest(
                 Request.Method.GET,
                 URL,
                 null,
-                new Response.Listener<JSONObject>() {
+                new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        Log.e("Rest Response", response.toString());
+                    public void onResponse(JSONArray responseArr) {
 
                         try {
-                            String responsePassword = response.getString("password");
-                            Log.e("checking pw", responsePassword + " " + loginPassword.getText().toString());
+                            JSONObject response = responseArr.getJSONObject(0);
 
-                            if(responsePassword.equals(loginPassword.getText().toString()))
-                            {
-                                Log.e("password part", "login successful");
-                                loggedIN = true;
+                            try {
+                                String responseUsername = response.getString("name");
+                                String responsePassword = response.getString("password");
+
+                                Log.e("checking pw", responsePassword + " " + loginPassword.getText().toString());
+
+                                if(responsePassword.equals(loginPassword.getText().toString()))
+                                {
+                                    Log.e("password matching: ", "login successful");
+                                    loggedIN = true;
+                                    startActivity(new Intent(MainActivity.this, RegisterScreen.class));
+                                }
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
 
-
-                        } catch (JSONException e) {
+                        }
+                        catch (JSONException e)
+                        {
                             e.printStackTrace();
                         }
 
+
+
                         if(loggedIN) {
                             loginStatus.setText("logged IN!");
-                            startActivity(new Intent(MainActivity.this, RegisterScreen.class));
+
 
                         }
                         else {
-                            loginStatus.setText("Wrong password");
+                            loginStatus.setText("Wrong password!");
                         }
 
                     }
@@ -116,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error)
                     {
                         Log.e("Rest Response",error.toString());
+                        loginStatus.setText("Non existing username!");
                     }
                 }
 
