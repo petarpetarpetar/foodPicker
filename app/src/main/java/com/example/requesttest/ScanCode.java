f89ab9525dd122e28cadd9e82879077cd3db6f1d
Vlada <vladapopov02@gmail.com>
2020-01-19 23:11:16 +0100

    barcode scanner added

---
+++ app/src/main/java/com/example/requesttest/ScanCode.java
@@ -0,0 +1,14 @@
+package com.example.requesttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScanCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_code);
+    }
+}
