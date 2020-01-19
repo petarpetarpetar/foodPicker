a5ed1fd8413e02608613f8895dba521a0cc94ae7
Vlada <vladapopov02@gmail.com>
2020-01-19 14:02:57 +0100

    latest

---
+++ app/src/main/java/com/example/requesttest/ItemClick.java
@@ -0,0 +1,14 @@
+package com.example.requesttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemClick extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_click);
+    }
+}
